2780026fc1bd83c6cf544a354b49b98bfe233f4b
ILQUIAS DOS SANTOS COSTA <98215417+kias223@users.noreply.github.com>
2023-05-20 14:04:41 -0300

    Update all-cats.js

--- frontend/js/all-cats.js
+++ frontend/js/all-cats.js
@@ -13,19 +13,12 @@ async function renderPage() {
     var catPhotosContainer = document.querySelector('.cat-photos-container')
     for (let info of dataResponse) {
         catPhotosContainer.innerHTML += `
-        <a href="../pages/cat-info.html" id="${info.id}" class="cat-card">
+        <a href="../pages/cat-info.html?id=${info.id}" class="cat-card">
                 <img src="${baseApi + info.top_cat_img}">
                 <p>${info.name}</p>
         </a>
         `
     }
-
-    var catCard = document.querySelectorAll('.cat-card')
-    catCard.forEach((element) => {
-        element.addEventListener('click', () => {
-            localStorage.setItem('card-render-id', element.id)
-        })
-    })
 
 }
 
